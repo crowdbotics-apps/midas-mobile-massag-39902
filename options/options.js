63a3f49f4ccf36ebf2a664846d764f8fbb006cc2
Crowdbotics <team@crowdbotics.com>
2023-04-13 17:17:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "midas-mobile-massag-39902",
+  url: "https://midas-mobile-massag-39902.botics.co",
+  api: "https://midas-mobile-massag-39902.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
