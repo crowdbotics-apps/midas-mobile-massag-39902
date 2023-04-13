ef61a262423a837952a64b064481eef38d82e165
Indira Allfree <indiraallfreelmt@gmail.com>
2023-04-13 10:35:25 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "midas-mobile-massag-39902",
-  url: "https://midas-mobile-massag-39902.botics.co",
-  api: "https://midas-mobile-massag-39902.botics.co/api/v1"
-}
+  "name": "midas-mobile-massag-39902",
+  "url": "https://midas-mobile-massag-39902.botics.co",
+  "api": "https://midas-mobile-massag-39902.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
